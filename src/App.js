c98130efd5b9c4eaea78e0bfce2a2b4b77001142
shreyasandurkar <35194160+shreyasandurkar@users.noreply.github.com>
2019-12-24 15:06:31 +0530

    final changes along with screen shot

--- src/App.js
+++ src/App.js
@@ -8,8 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to Google Code-in 2019
-          Edit <code>src/App.js</code> and save to reload.
+          Amaze App
         </p>
         <a
           className="App-link"
@@ -17,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Welcome to Google-Code-in 2019
         </a>
       </header>
     </div>
